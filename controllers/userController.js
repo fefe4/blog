e0277d08deec2ad4da5ece93763e82ccfe074c54
fefe4 <mynameisfefe@protonmail.com>
2021-06-17 23:49:20 -0300

    changed user sign up

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,22 +57,6 @@ exports.user_signUp = [
   },
 ];
 
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    });
-  })
-);
 
 
 app.use(passport.initialize());
@@ -80,46 +64,40 @@ app.use(passport.initialize());
 exports.user_logIn = [
   body("username", "username, can not be empty ")
     .trim()
-    .isLength({ min: 3 })
-    .withMessage("must be at least 3 characters long")
     .escape(),
 
   //password
   body("password", "password can not be empty")
     .trim()
-    .isLength({ min: 3 })
-    .withMessage("must be at least 3 characters long")
-    .matches(/[0-9]/)
-    .withMessage("must contain a number")
-    .matches(/[A-Z]/)
-    .withMessage("must contain a capital letter")
     .escape(),
 
-
    function (req, res) {
-    passport.authenticate("local", { session: false }, (err, user, info) => {
-      console.log(user)
-      if (err || !user) {
-        return res.status(400).json({
-          message: "Something is not right",
-          user: user,
-         
-        });
+    User.findOne({ 'username': req.body.username }, (err, user) => {
+      if (err) { 
+        return res.json(err);
       }
-      req.login(user, { session: false }, (err) => {
-        if (err) {
-          console.log("333")
-          res.send(err);
-        }        
-        // generate a signed son web token with the contents of user object and return it in the response
+      if (!user) {
+        return res.json( { message: "Incorrect username" });
+      }
+      if (user.password !== req.body.password) {
+        
+      }
+      bcrypt.compare(req.body.password, user.password, (err, data) => {
+        if (data) {
+       // generate a signed son web token with the contents of user object and return it in the response
         console.log("444")
-        const token = jwt.sign(user.toJSON() , "your_jwt_secret");
-       
-        return res.json({ user, token });
+        const token = jwt.sign({username:user.username} , "your_jwt_secret");
+        const a =  user.username
+        return res.json({user:a , token });
+        } else {
+          // passwords do not match!
 
+          return res.send("pepe")
+        }
+      })
       });
-    }) (req, res);
-  },
+
+    }
 ];
 
 // I need to pass my token as a header && and set it in local storage for React 
